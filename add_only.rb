b6cbe10819a9aeb836ffb50a60f98700452013f8
JBrow95 <browningjustin95@gmail.com>
2018-06-11 11:07:20 -0400

    added a print method

--- add_only.rb
+++ add_only.rb
@@ -1,14 +1,16 @@
 def sumof()	# function
 
-	p "Enter a value: "
+	p "Enter a value"
 
 	x = gets.chomp.to_i	# user imputs an integer and that integer equals the var x
 
-	p "Enter another value: "
+	p "Enter another value"
 
 	y = gets.chomp.to_i # user imputs an integer and that integer equals the var x
 
 	z = x + y # adds the input from var x, y and add them together.
+
+	p "Your answer is" 
 
 	puts "#{z}" # prints var
 end

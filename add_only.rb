6923e46369c49ce8df81a6a05de81f0643dbcd42
JBrow95 <browningjustin95@gmail.com>
2018-06-11 10:57:39 -0400

    create print methods to tell the user when to input

--- add_only.rb
+++ add_only.rb
@@ -1,6 +1,10 @@
 def sumof()	# function
 
+	p "Enter a value: "
+
 	x = gets.chomp.to_i	# user imputs an integer and that integer equals the var x
+
+	p "Enter another value: "
 
 	y = gets.chomp.to_i # user imputs an integer and that integer equals the var x
 
